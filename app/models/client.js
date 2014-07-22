f3d1ce1212b340bb59256edb7f28e4dd428d0e99
Dave Boling <kadowki@gmail.com>
2014-07-21 23:15:01 -0500

    Completed the stock program to pre-instructor specs, may need changes to http requests

--- app/models/client.js
+++ app/models/client.js
@@ -7,6 +7,7 @@ function Client(name, balance){
 	this.name = name;
 	this.balance = balance;
 	this.portfolio = new Portfolio(this.name + '\'s portfolio');
+	this.totalWorth = 0;
 }
 
 
@@ -24,11 +25,42 @@ if(this.balance >= totalPrice){
 };
 
 Client.prototype.sell = function(symbol, quantity){
-	
+	var index = findStock(this.portfolio.stocks, symbol);
+	var currentPrice = getPrice(symbol) * quantity;
+
+  if(index >= 0){
+    this.portfolio.stocks[index].count -= quantity;
+    this.balance += currentPrice;
+
+    if(this.portfolio.stocks[index].count <= 0){
+      this.portfolio.stocks.splice(index, 1);
+    }
+  }
+
+}
+
+Client.prototype.position = function(){
+	var sum = this.balance;
+	for(var i = 0; i < this.portfolio.stocks.length; i++){
+		sum += this.portfolio.stocks[i].count * getPrice(this.portfolio.stocks[i].symbol);
+	}
+
+	this.totalWorth = sum;
+	return this.totalWorth;
+	console.log('Your total worth is: ' + sum);
 }
 
 
 /* PRIVATE FUNCTION */
+function findStock(stocks, symbol){
+  for(var i = 0; i < stocks.length; i++){
+    if(stocks[i].symbol === symbol.toUpperCase()){
+      return i;
+    }
+  }
+
+  return -1;
+}
 
 function getPrice(symbol){
 //Simple get request that is persistent
@@ -36,7 +68,7 @@ function getPrice(symbol){
  	var url = 'http://dev.markitondemand.com/Api/v2/Quote/json?symbol=' + symbol.toUpperCase();
  	var stock = request(url);
  	stock = JSON.parse(stock.body);
- 	console.log('Still working! ' + stock.LastPrice);
+ 	//console.log('Still working! ' + stock.LastPrice);
 
 	return stock.LastPrice;
 }
--- test/unit/clientTest.js
+++ test/unit/clientTest.js
@@ -9,12 +9,30 @@ var Client = require('../../app/models/client');
 
 describe('Client', function(){
   describe('.purchase', function(){
-    it('allow purchase of stocks and addition to portfolio.', function(){
+    it('should allow purchase of stocks and addition to portfolio.', function(){
      var bob = new Client('Bob Smith', 10000);
 
         bob.purchase('aapl', 50);       
         expect(bob.balance).to.be.closeTo(5302.75, 10);
         expect(bob.portfolio.stocks.length).to.equal(1);
+    });
+  });
+  describe('.sell', function() {
+      it('should allow the sale of stock if available', function(){
+        var bob = new Client('Bob Smith', 10000);
+        bob.purchase('aapl', 50); 
+        bob.sell('aapl', 40);
+
+        expect(bob.balance).to.be.closeTo(9060.55, 20);
+      });
+  });
+  describe('.position', function() {
+    it('should output the total value of all stocks', function(){
+      var bob = new Client('Bob Smith', 10000);
+      bob.portfolio.add('aapl', 100);
+      expect(bob.portfolio.stocks[0].symbol).to.equal('AAPL');
+      expect(bob.portfolio.stocks[0].count).to.equal(100);
+      expect(bob.position()).to.be.closeTo(19394.5, 20);
     });
   });
 });
